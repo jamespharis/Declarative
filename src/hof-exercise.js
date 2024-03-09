04388ff84d680cda7e9f189b31e715055c45a42f
James Pharis <jamesmpharis@gmail.com>
2024-03-08 19:21:34 -0500

    Declarative Assignment Completed

--- src/hof-exercise.js
+++ src/hof-exercise.js
@@ -2,6 +2,8 @@
 // EXAMPLES
 /**********/
 
+import { number } from "jsverify";
+
 // numbers: [number]
 const numbers = [1, 2, 3];
 
@@ -27,28 +29,39 @@ const calculateTotalImperative = (items, tax) => {
 /**********/
 // ASSIGNMENT
 /**********/
+// blah: parameters to take in -> return type
+
+/* - - - - - - - - - - IMPLEMENTATION - - - - - - - - - - */
 
 // prices: (items: [{price: number}]) -> [number]
-const prices = undefined; // TODO - Implementation
+const prices = items => items.map(item => item.price); 
+// Take in items, psuedo-for loop through them, for each item, return its price... map method returns all the data in an array
+// Passing in a lambda function to take every item in the items list and return just the price (can think of price as nested, now)
 
-// sum: (numbers: [number]) -> number
-const sum = undefined; // TODO - Implementation
+// sum: (numbers: [number]) -> number       // reduce(accumulator, currentvalue)
+const sum = numbers => numbers.reduce((ACC, CURR) => ACC + CURR, 0); 
+// Take in numbers array, return ACC as the total + CURR as new value, and 0 as the initial value
 
 // selectTaxable: (items: [{taxable: boolean}]) -> [{taxable: boolean}]
-const selectTaxable = undefined; // TODO - Implementation
+const selectTaxable = items => items.filter(item => item.taxable); 
+// Take in items, create new variable 'item' and return the item as the boolean inside of that item (object)
 
 // applyTax: (prices: [number], tax: number) -> [number]
-const applyTax = undefined; // TODO - Implementation
+const applyTax = (prices,tax) => prices.map(price => price * tax); 
+// Take in prices array & tax, create new variable 'price,' which is our current object's price, then multiply that price by the tax
 
-// baseSum: TODO - Type Signature
+
+
+
+/* - - - - - IMPLEMENT TYPE SIGNATURES - - - - - */  // EX:  baseSum: TODO - Type Signature
+// baseSum: (items: [{price: number}]) -> number
 const baseSum = items => sum(prices(items));
 
-// taxSum: TODO - Type Signature
+// taxSum: (items: [{price: number, taxable: boolean}], tax: number ) -> number
 const taxSum = (items, tax) => sum(applyTax(prices(selectTaxable(items)), tax));
 
-// calculateTotalDeclarative: TODO - Type Signature
-const calculateTotalDeclarative = (items, tax) =>
-  baseSum(items) + taxSum(items, Math.abs(tax));
+// calculateTotalDeclarative: (items: [{price: number, taxable: boolean}], tax: number ) -> number
+const calculateTotalDeclarative = (items, tax) => baseSum(items) + taxSum(items, Math.abs(tax));
 
 export default {
   prices,
